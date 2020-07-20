94afb210a3fd9e28b4eacdb6ddf6af7ac96f11ff
layanmeta <66564396+layanmeta@users.noreply.github.com>
2020-07-20 15:10:45 +0300

    Add files via upload

---
+++ Citizen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_17
+{
+    class Citizen
+    {
+        public string m_name;
+        public Citizen[] m_children;
+        public readonly int m_fatherID;
+        public readonly int m_ID;
+        public static int m_number_of_current_citizens;
+        public const int m_max_numer_of_citizens =6000;
+
+        public Citizen(string m_name, int m_fatherID)
+        {
+            this.m_name = m_name;
+            this.m_fatherID = m_fatherID;
+        }
+
+        public Citizen(string m_name, int m_fatherID, int m_ID) : this(m_name, m_fatherID)
+        {
+            this.m_ID = m_ID;
+        }
+
+
+        static void Print_Number_Of_Citizens()
+        {
+            Console.WriteLine($"The number of citizens is : {m_max_numer_of_citizens} ");
+            
+        }
+
+        static bool Reached_Half_Of_Maximum_Citizens()
+        {
+            
+            if (m_number_of_current_citizens == m_max_numer_of_citizens / 2) 
+            {
+                return true;
+            }
+             else
+            {
+                return false;
+            }
+            
+        }
+        public void Print_ID()
+        {
+
+        }
+
+        public void Pring_Gap_Between_My_ID_And_Father()
+        {
+            Console.WriteLine($" The gabe between my ID and father's is : {m_ID - m_fatherID}");
+        }
+
+        public void Set_Children(Citizen[])
+        {
+            Citizen[] children = new Citizen[]
+            {
+
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_17
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            Citizen father = new Citizen("father", 15);
+
+            Citizen kid1 = new Citizen("kid1", 15, 40);
+            Citizen kid2 = new Citizen("kid2", 15, 41);
+            Citizen kid3 = new Citizen("kid3", 15, 42);
+
+
+        }
+    }
+}
